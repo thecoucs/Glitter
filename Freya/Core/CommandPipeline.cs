6d3c13edff7518951ca22200b4d1eb22f5c8f7ae
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-27 16:11:06 -0600

    Created command pipeline shell.

---
+++ Freya/Core/CommandPipeline.cs
@@ -0,0 +1,11 @@
+﻿using Mauve.Patterns;
+using Mauve.Runtime.Processing;
+
+namespace Freya.Core
+{
+    internal class CommandPipeline : IPipeline<IBotCommand>
+    {
+        public void Run(IMiddleware<IBotCommand> middleware) => throw new NotImplementedException();
+        public IPipeline<IBotCommand> Use(IMiddleware<IBotCommand> middleware) => throw new NotImplementedException();
+    }
+}
