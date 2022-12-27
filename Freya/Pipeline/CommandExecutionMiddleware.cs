286179d46602ee1f86aced13bc18e427fa62a21e
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-27 16:11:36 -0600

    Created basic execution middleware

---
+++ Freya/Pipeline/CommandExecutionMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using Freya.Core;
+
+using Mauve.Patterns;
+
+namespace Freya.Pipeline
+{
+    internal class CommandExecutionMiddleware : IMiddleware<IBotCommand>
+    {
+        public void Invoke(IBotCommand input, MiddlewareDelegate<IBotCommand> next) => throw new NotImplementedException();
+        public Task InvokeAsync(IBotCommand input, MiddlewareDelegate<IBotCommand> next) => throw new NotImplementedException();
+        public Task InvokeAsync(IBotCommand input, MiddlewareDelegate<IBotCommand> next, CancellationToken cancellationToken) => throw new NotImplementedException();
+    }
+}
