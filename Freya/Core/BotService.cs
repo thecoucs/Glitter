a49dbfcc85b3e3ca26cb04b0f19c5ee083891349
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-27 16:11:25 -0600

    Created bot service shell.

---
+++ Freya/Core/BotService.cs
@@ -0,0 +1,42 @@
+﻿using Mauve.Patterns;
+using Mauve.Runtime.Processing;
+using Mauve.Runtime.Services;
+
+namespace Freya.Core
+{
+    internal abstract class BotService : IPipelineService<IBotCommand>
+    {
+
+        #region Fields
+
+        private IDependencyCollection? _dependencies;
+        private IPipeline<IBotCommand>? _commandPipeline;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+
+        public abstract void Configure(IDependencyCollection dependencies, IPipeline<IBotCommand> pipeline);
+        public async Task Start()
+        {
+
+            // Create the initial dependency collection.
+            _dependencies = new DependencyCollection();
+
+            // Create the initial command pipeline.
+            _commandPipeline = new CommandPipeline();
+
+            // Allow implementing services to configure their dependencies and custom pipelines.
+            Configure(_dependencies, _commandPipeline);
+
+            // Add the basic execution pipeline.
+        }
+
+        #endregion
+
+    }
+}
