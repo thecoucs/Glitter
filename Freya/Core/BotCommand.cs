d7c95c7b33393b9c6297f447a9475fc3cc2f5eef
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 08:24:31 -0600

    Added some execution logic for bot commands.

--- Freya/Core/BotCommand.cs
+++ Freya/Core/BotCommand.cs
@@ -1,4 +1,7 @@
-﻿using Mauve.Runtime;
+﻿using Mauve;
+using Mauve.Extensibility;
+using Mauve.Math;
+using Mauve.Runtime;
 
 namespace Freya.Core
 {
@@ -7,6 +10,7 @@ namespace Freya.Core
 
         #region Fields
 
+        private readonly string _id;
         private readonly ILogger<LogEntry> _logger;
 
         #endregion
@@ -23,21 +27,47 @@ namespace Freya.Core
 
         public BotCommand(string key, string displayName, string description, ILogger<LogEntry> logger)
         {
+            _logger = logger;
+            _id = Guid.NewGuid().GetHashCode(NumericBase.Hexadecimal);
+
             Key = key;
             DisplayName = displayName;
             Description = description;
-            _logger = logger;
         }
 
         #endregion
 
         #region Public Methods
 
-        public abstract void Execute();
+        public void Execute()
+        {
+            bool encounteredErrors = false;
+            Log(EventType.Information, $"Executing command '{DisplayName}'...");
+            try
+            {
+                Work();
+            } catch (Exception e)
+            {
+                Log(EventType.Exception, $"An unexpected exception occurred during execution. {e.Message}");
+            } finally
+            {
+                // Set the event type for the completion message.
+                EventType type = encounteredErrors
+                    ? EventType.Error
+                    : EventType.Success;
+
+                // Log the completion message.
+                Log(type, "Execution complete.");
+            }
+        }
 
         #endregion
 
         #region Protected Methods
+
+        protected abstract void Work();
+        protected void Log(EventType eventType, string message) =>
+            _logger.Log(new LogEntry(eventType, $"{_id}: {message}"));
 
         #endregion
 
