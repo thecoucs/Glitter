5391cc515cc53a69d65625656399b73244043ac1
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 07:56:21 -0600

    Created log entry.

---
+++ Freya/Core/LogEntry.cs
@@ -0,0 +1,29 @@
+﻿using Mauve;
+
+namespace Freya.Core
+{
+    internal class LogEntry
+    {
+
+        #region Properties
+
+        public EventType Type { get; set; }
+        public string Message { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public LogEntry() :
+            this(EventType.None, string.Empty)
+        { }
+        public LogEntry(EventType type, string message)
+        {
+            Type = type;
+            Message = message;
+        }
+
+        #endregion
+
+    }
+}
