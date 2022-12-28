bcbf8b05b81a8accb0eb94a90d3e3cd7fed95d01
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 08:27:57 -0600

    Added execution logic to middleware.

--- Freya/Pipeline/CommandExecutionMiddleware.cs
+++ Freya/Pipeline/CommandExecutionMiddleware.cs
@@ -4,10 +4,43 @@ using Mauve.Patterns;
 
 namespace Freya.Pipeline
 {
+    /// <summary>
+    /// Represents an implementation of <see cref="IMiddleware{T}"/> that executes <see cref="BotCommand"/> instances.
+    /// </summary>
+    /// <inheritdoc/>
     internal class CommandExecutionMiddleware : IMiddleware<BotCommand>
     {
-        public void Invoke(BotCommand input, MiddlewareDelegate<BotCommand> next) => throw new NotImplementedException();
-        public Task InvokeAsync(BotCommand input, MiddlewareDelegate<BotCommand> next) => throw new NotImplementedException();
-        public Task InvokeAsync(BotCommand input, MiddlewareDelegate<BotCommand> next, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CommandExecutionMiddleware"/>.
+        /// </summary>
+        public CommandExecutionMiddleware() { }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc/>
+        public void Invoke(BotCommand input, MiddlewareDelegate<BotCommand> next)
+        {
+            try
+            {
+                input.Execute();
+            } catch { } finally
+            {
+                _ = next?.Invoke(input);
+            }
+        }
+        /// <inheritdoc/>
+        public async Task InvokeAsync(BotCommand input, MiddlewareDelegate<BotCommand> next) =>
+            await InvokeAsync(input, next, CancellationToken.None);
+        /// <inheritdoc/>
+        public async Task InvokeAsync(BotCommand input, MiddlewareDelegate<BotCommand> next, CancellationToken cancellationToken) =>
+            await Task.Run(() => Invoke(input, next), cancellationToken);
+
+        #endregion
+
     }
 }
