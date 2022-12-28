5aff6a326f80b3dcd86ef21fbd5993ff041cbb36
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 10:58:38 -0600

    Added cancellation token to Freya's brain.

--- Freya/Brain.cs
+++ Freya/Brain.cs
@@ -9,25 +9,38 @@ namespace Freya
         #region Fields
 
         private readonly List<BotService> _services;
+        private readonly CancellationToken _cancellationToken;
+        private readonly CancellationTokenSource _cancellationTokenSource;
 
         #endregion
 
         #region Constructor
 
-        public Brain() =>
+        public Brain()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _cancellationToken = _cancellationTokenSource.Token;
             _services = new List<BotService>();
+        }
 
         #endregion
 
         #region Public Methods
 
+        public void Cancel() =>
+            _cancellationTokenSource.Cancel();
         public void Initialize() =>
-            _services.Add(new TestService());
+            _services.Add(new TestService(_cancellationToken));
         public async Task Start()
         {
-            // Start all of the bot services.
+            // Cancel if requested, otherwise start each service.
+            _cancellationToken.ThrowIfCancellationRequested();
             foreach (BotService service in _services)
+            {
+                // Cancel if requested, otherwise start the service.
+                _cancellationToken.ThrowIfCancellationRequested();
                 await service.Start();
+            }
         }
 
         #endregion
