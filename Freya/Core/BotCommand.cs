b76ed9dc51312973d8c213faf9ed98be3fe2ed18
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 08:03:20 -0600

    Added basic properties to bot command.

--- Freya/Core/BotCommand.cs
+++ Freya/Core/BotCommand.cs
@@ -2,5 +2,25 @@
 {
     internal class BotCommand
     {
+
+        #region Properties
+
+        public string Key { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public BotCommand(string key, string displayName, string description)
+        {
+            Key = key;
+            DisplayName = displayName;
+            Description = description;
+        }
+
+        #endregion
+
     }
 }
