c3f19745eb8f77561b338f2bbcea18defbb95d3b
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 07:47:12 -0600

    Added services field to brain.

--- Freya/Brain.cs
+++ Freya/Brain.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Freya.Core;
 
 namespace Freya
 {
     internal class Brain
     {
+
+        #region Fields
+
+        private readonly List<BotService> _services;
+
+        #endregion
+
+        #region Constructor
+
+        public Brain() =>
+            _services = new List<BotService>();
+
+        #endregion
+
     }
 }
