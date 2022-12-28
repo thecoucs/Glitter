d49cba573c6bdbccfab75728e9c8e717accc4873
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 10:47:19 -0600

    Added documentation to LogEntry.

--- Freya/Core/LogEntry.cs
+++ Freya/Core/LogEntry.cs
@@ -2,21 +2,38 @@
 
 namespace Freya.Core
 {
+    /// <summary>
+    /// Represents a log entry.
+    /// </summary>
     internal class LogEntry
     {
 
         #region Properties
 
+        /// <summary>
+        /// The <see cref="EventType"/> of the entry.
+        /// </summary>
         public EventType Type { get; set; }
+        /// <summary>
+        /// The message the entry is for.
+        /// </summary>
         public string Message { get; set; }
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Creates a new instance of <see cref="LogEntry"/>.
+        /// </summary>
         public LogEntry() :
             this(EventType.None, string.Empty)
         { }
+        /// <summary>
+        /// Creates a new instance of <see cref="LogEntry"/>.
+        /// </summary>
+        /// <param name="type">The <see cref="EventType"/> of the entry.</param>
+        /// <param name="message">The message the entry is for.</param>
         public LogEntry(EventType type, string message)
         {
             Type = type;
