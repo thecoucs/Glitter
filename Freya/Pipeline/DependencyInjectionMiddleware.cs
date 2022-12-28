c7ac8c4022d6a07612dd08915014dd523f1cbcb4
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 08:15:15 -0600

    Created DI middleware.

---
+++ Freya/Pipeline/DependencyInjectionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Freya.Core;
+
+using Mauve.Patterns;
+
+namespace Freya.Pipeline
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IMiddleware{T}"/> that injects dependencies into <see cref="BotCommand"/> instances.
+    /// </summary>
+    /// <inheritdoc/>
+    internal class DependencyInjectionMiddleware : IMiddleware<BotCommand>
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DependencyInjectionMiddleware"/>.
+        /// </summary>
+        public DependencyInjectionMiddleware(IDependencyCollection dependencies)
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc/>
+        public void Invoke(BotCommand input, MiddlewareDelegate<BotCommand> next)
+        {
+
+        }
+        /// <inheritdoc/>
+        public async Task InvokeAsync(BotCommand input, MiddlewareDelegate<BotCommand> next) =>
+            await InvokeAsync(input, next, CancellationToken.None);
+        /// <inheritdoc/>
+        public async Task InvokeAsync(BotCommand input, MiddlewareDelegate<BotCommand> next, CancellationToken cancellationToken) =>
+            await Task.Run(() => Invoke(input, next), cancellationToken);
+
+        #endregion
+
+    }
+}
