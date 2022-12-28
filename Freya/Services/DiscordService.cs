fa9ee5d7bc55155fee0de2f7109d1b2c74a58516
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 15:20:21 -0600

    Created a Discord service shell.

---
+++ Freya/Services/DiscordService.cs
@@ -0,0 +1,46 @@
+﻿using Freya.Core;
+using Freya.Runtime;
+
+using Mauve;
+using Mauve.Patterns;
+using Mauve.Runtime.Processing;
+
+namespace Freya.Services
+{
+    internal class DiscordService : BotService
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
+        public DiscordService(CancellationToken cancellationToken) :
+            base(new ConsoleLogger(), cancellationToken)
+        { }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Configure(IDependencyCollection dependencies, IPipeline<BotCommand> pipeline)
+        {
+
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <inheritdoc/>
+        protected override async Task Run(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Log(EventType.Information, "The test service is alive.");
+        }
+
+        #endregion
+
+    }
+}
