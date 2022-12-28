cd984b2942505ccf0827abf318309180aba9bfb7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-28 09:58:05 -0600

    Added a test service.

---
+++ Freya/Services/TestService.cs
@@ -0,0 +1,41 @@
+﻿using Freya.Core;
+using Freya.Runtime;
+
+using Mauve;
+using Mauve.Patterns;
+using Mauve.Runtime.Processing;
+
+namespace Freya.Services
+{
+    internal class TestService : BotService
+    {
+
+        #region Constructor
+
+        public TestService() :
+            base(new ConsoleLogger())
+        { }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Configure(IDependencyCollection dependencies, IPipeline<BotCommand> pipeline)
+        {
+
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override async Task Run()
+        {
+            Log(EventType.Information, "The test service is alive.");
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        #endregion
+
+    }
+}
