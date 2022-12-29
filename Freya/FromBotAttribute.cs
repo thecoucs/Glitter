840f2631fb666879b07f30cd75f59301af79a2b4
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 17:02:07 -0600

    Created from bot attribute.

---
+++ Freya/FromBotAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Freya
+{
+    /// <summary>
+    /// Allows specification that a member comes from the bot.
+    /// </summary>
+    internal class FromBotAttribute : Attribute
+    {
+    }
+}
