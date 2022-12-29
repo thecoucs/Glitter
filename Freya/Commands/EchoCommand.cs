d22e870ec8c7b5cee5478325d997f48348d4b9e6
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 16:53:49 -0600

    Added echo command for testing.

---
+++ Freya/Commands/EchoCommand.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+using Discord.Commands;
+
+using Mauve.Runtime;
+
+namespace Freya.Commands
+{
+    [Alias("echo")]
+    internal class EchoCommand : BotCommand
+    {
+
+        #region Fields
+
+        private readonly string _input;
+
+        #endregion
+
+        #region Constructor
+
+        public EchoCommand([FromBot] string input, ILogger<LogEntry> logger) :
+            base("Echo", "Echoes the specified input back to the user.", logger) =>
+            _input = input;
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override async Task Work(CancellationToken cancellationToken)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(_input);
+            _ = new CommandResponse(data);
+            //await invokingService.SendResponse(response, cancellationToken);
+            await Task.CompletedTask; // TODO: remove.
+        }
+
+        #endregion
+
+    }
+}
