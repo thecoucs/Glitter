b60b2c5b2be829f93dff331732a0f6bb03a29e61
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 17:03:25 -0600

    Cleaned up color code.

--- Freya/Runtime/ConsoleLogger.cs
+++ Freya/Runtime/ConsoleLogger.cs
@@ -1,31 +1,21 @@
-﻿using Freya.Core;
-
-using Mauve;
+﻿using Mauve;
 using Mauve.Runtime;
 
 namespace Freya.Runtime
 {
     internal class ConsoleLogger : ILogger<LogEntry>
     {
+
+        #region Public Methods
+
         public void Log(LogEntry input)
         {
             // Capture the incoming foreground color.
             ConsoleColor incomingColor = Console.ForegroundColor;
-
             try
             {
-                // Determine the color we should write with.
-                ConsoleColor writeColor = input.Type.HasFlag(EventType.Success)
-                    ? ConsoleColor.Green
-                    : input.Type.HasFlag(EventType.Warning)
-                        ? ConsoleColor.Yellow
-                        : input.Type.HasFlag(EventType.Error) ||
-                            input.Type.HasFlag(EventType.Exception)
-                            ? ConsoleColor.Red
-                            : incomingColor;
-
                 // Write the message.
-                Console.ForegroundColor = writeColor;
+                Console.ForegroundColor = GetForecolor(input.Type, incomingColor);
                 Console.WriteLine(input.Message);
             } catch (Exception e)
             {
@@ -40,5 +30,20 @@ namespace Freya.Runtime
             await LogAsync(input, CancellationToken.None);
         public async Task LogAsync(LogEntry input, CancellationToken cancellationToken) =>
             await Task.Run(() => Log(input), cancellationToken);
+
+        #endregion
+
+        #region Private Methods
+
+        private ConsoleColor GetForecolor(EventType type, ConsoleColor defaultColor) => type.HasFlag(EventType.None) switch
+        {
+            true when type.HasFlag(EventType.Success) => ConsoleColor.Green,
+            true when type.HasFlag(EventType.Warning) => ConsoleColor.Yellow,
+            true when type.HasFlag(EventType.Error) || type.HasFlag(EventType.Exception) => ConsoleColor.Red,
+            _ => defaultColor
+        };
+
+        #endregion
+
     }
 }
