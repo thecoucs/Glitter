6c4bc6e11fb879484339a02b3bfe867b9cd915df
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 11:42:03 -0600

    Added configuration to startup logic.

--- Freya/Program.cs
+++ Freya/Program.cs
@@ -1,8 +1,33 @@
 ﻿using Freya;
 
+using Microsoft.Extensions.Configuration;
+
 Console.WriteLine($"Starting Freya.");
+
+// Validate the base directory.
+string baseDirectory = AppContext.BaseDirectory;
+if (string.IsNullOrWhiteSpace(baseDirectory))
+{
+    Console.WriteLine("Unable to start. The base directory is null or empty.");
+    return;
+}
+
+// Validate the parent directory.
+DirectoryInfo? parentDirectory = Directory.GetParent(baseDirectory);
+if (parentDirectory is null)
+{
+    Console.WriteLine("Unable to start. The parent directory is null.");
+    return;
+}
+
+// Build configuration
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(parentDirectory.FullName)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.development.json", optional: true)
+    .Build();
 
 // Create the brain and wake it up.
 var brain = new Brain();
-brain.Initialize();
+brain.Configure(configuration);
 await brain.Start();
