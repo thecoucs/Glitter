7124ea3b8d8d48d09ae0eb54aff43d1be7be581c
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 11:40:42 -0600

    Added the ability for bots to have settings.

---
+++ Freya/Core/BotServiceWithSettings.cs
@@ -0,0 +1,28 @@
+﻿using Mauve.Runtime;
+
+namespace Freya.Core
+{
+    internal abstract class BotService<TSettings> : BotService
+    {
+
+        #region Properties
+
+        protected TSettings Settings { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
+        public BotService(TSettings settings, ILogger<LogEntry> logger, CancellationToken cancellationToken) :
+            base(logger, cancellationToken) =>
+            Settings = settings;
+
+        #endregion
+
+    }
+}
