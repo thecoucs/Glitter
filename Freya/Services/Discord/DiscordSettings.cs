ed81755477b0009a4976425c46332992cd3bb68b
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 11:40:58 -0600

    Created discord settings.

---
+++ Freya/Services/Discord/DiscordSettings.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Freya.Services.Discord
+{
+    internal class DiscordSettings
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; } = string.Empty;
+    }
+}
