7fe2ec2dbdb216427b2218f693f766a54194f3b7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 12:29:01 -0600

    Updated documentation.

--- src/Services/Discord/DiscordSettings.cs
+++ src/Services/Discord/DiscordSettings.cs
@@ -2,8 +2,14 @@
 
 namespace Freya.Services.Discord
 {
+    /// <summary>
+    /// Represents a collection of settings for the <see cref="DiscordService"/>.
+    /// </summary>
     internal class DiscordSettings
     {
+        /// <summary>
+        /// The token used to authenticate with Discord.
+        /// </summary>
         [JsonProperty("token")]
         public string Token { get; set; } = string.Empty;
     }
