270db5471460616e39e0b7d47ef27999b4ec435d
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 11:38:21 -0600

    Added documentation.

--- src/Core/Brain.cs
+++ src/Core/Brain.cs
@@ -6,12 +6,19 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Freya.Core
 {
+    /// <summary>
+    /// Represents Freya's central processing system.
+    /// </summary>
     internal class Brain
     {
         private readonly List<BotService> _bots;
         private readonly ServiceFactory _serviceFactory;
         private readonly CancellationToken _cancellationToken;
         private readonly CancellationTokenSource _cancellationTokenSource;
+        /// <summary>
+        /// Creates a new instance of <see cref="Brain"/>.
+        /// </summary>
+        /// <param name="serviceFactory">The <see cref="ServiceFactory"/> for discovering <see cref="BotService"/> instances.</param>
         public Brain(ServiceFactory serviceFactory)
         {
             _cancellationTokenSource = new CancellationTokenSource();
@@ -19,8 +26,15 @@ namespace Freya.Core
             _bots = new List<BotService>();
             _serviceFactory = serviceFactory;
         }
+        /// <summary>
+        /// Cancels processing for the <see cref="Brain"/>.
+        /// </summary>
         public void Cancel() =>
             _cancellationTokenSource.Cancel();
+        /// <summary>
+        /// Configures the <see cref="Brain"/> and discovered <see cref="BotService"/> instances.
+        /// </summary>
+        /// <param name="services">The service collection the <see cref="Brain"/> and <see cref="BotService"/> instances can add to.</param>
         public void Configure(IServiceCollection services)
         {
             // Cancel if requested, otherwise discover and configure services.
@@ -31,6 +45,10 @@ namespace Freya.Core
                 _bots.Add(bot);
             }
         }
+        /// <summary>
+        /// Starts the brain.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
         public async Task Start()
         {
             // Cancel if requested, otherwise start each service.
