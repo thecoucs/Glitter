0f1245af8a177625aec6f8aadd910cc6053607c0
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 12:37:00 -0600

    Updated documentation.

--- src/Services/BotService.cs
+++ src/Services/BotService.cs
@@ -11,29 +11,21 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Freya.Services
 {
+    /// <summary>
+    /// Represents a service for integrating Freya with a specific provider.
+    /// </summary>
     internal abstract class BotService
     {
-
-        #region Fields
-
         private readonly string _name;
         private readonly ILogger<LogEntry> _logger;
         private readonly CancellationToken _cancellationToken;
-
-        #endregion
-
-        #region Properties
-
         protected CommandFactory CommandFactory { get; set; }
-
-        #endregion
-
-        #region Constructor
-
         /// <summary>
-        /// 
+        /// Creates a new <see cref="BotService"/> instance.
         /// </summary>
-        /// <param name="logger"></param>
+        /// <param name="name">The name of the service.</param>
+        /// <param name="logger">The logger to be utilized by the service.</param>
+        /// <param name="commandFactory">The <see cref="Freya.Runtime.CommandFactory"/> instance the service should use when creating commands.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
         public BotService(string name, ILogger<LogEntry> logger, CommandFactory commandFactory, CancellationToken cancellationToken)
         {
@@ -42,11 +34,11 @@ namespace Freya.Services
             _cancellationToken = cancellationToken;
             CommandFactory = commandFactory;
         }
-
-        #endregion
-
-        #region Public Methods
-
+        /// <summary>
+        /// Configures the <see cref="BotService"/>.
+        /// </summary>
+        /// <param name="services">The service collection to add to.</param>
+        /// <param name="pipeline">The pipeline to register middleware with.</param>
         public void Configure(IServiceCollection services, IPipeline<Command> pipeline)
         {
             // Allow the concrete service to configure itself.
@@ -56,6 +48,10 @@ namespace Freya.Services
             var executor = new CommandExecutionMiddleware(_cancellationToken);
             pipeline.Run(executor);
         }
+        /// <summary>
+        /// Starts the <see cref="BotService"/>.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
         public async Task Start()
         {
             // Cancel if requested, otherwise create the command pipeline and dependency collection.
@@ -73,25 +69,28 @@ namespace Freya.Services
             }, _cancellationToken);
             await Task.CompletedTask;
         }
-
-        #endregion
-
-        #region Protected Methods
-
+        /// <summary>
+        /// Configures the service.
+        /// </summary>
+        /// <param name="services">The service collection to add to.</param>
+        /// <param name="pipeline">The pipeline to register middleware with.</param>
         protected abstract void ConfigureService(IServiceCollection services, IPipeline<Command> pipeline);
         /// <summary>
-        /// 
+        /// Runs the primary work for the service.
         /// </summary>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
-        /// <returns>A <see cref="Task"/> describing the state of execution.</returns>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
         protected abstract Task Run(CancellationToken cancellationToken);
+        /// <summary>
+        /// Records a message with a specified <see cref="EventType"/>.
+        /// </summary>
+        /// <param name="eventType">The <see cref="EventType"/> associated with the message.</param>
+        /// <param name="message">The message to log.</param>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
         protected async Task Log(EventType eventType, string message)
         {
             _cancellationToken.ThrowIfCancellationRequested();
             await _logger.LogAsync(new LogEntry(eventType, $"{_name}: {message}"));
         }
-
-        #endregion
-
     }
 }
