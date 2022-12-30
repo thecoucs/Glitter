64361ef623110048214e9db5c0e51a22bb3c6e7d
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 12:18:20 -0600

    Added documentation to request parser.

--- src/Runtime/RequestParser.cs
+++ src/Runtime/RequestParser.cs
@@ -3,11 +3,24 @@
 using Mauve.Extensibility;
 namespace Freya.Runtime
 {
+    /// <summary>
+    /// Represents a parser responsible for creating <see cref="CommandRequest"/> instances.
+    /// </summary>
     internal class RequestParser
     {
         private readonly string _commandToken;
+        /// <summary>
+        /// Creates a new <see cref="RequestParser"/> instance.
+        /// </summary>
+        /// <param name="commandToken">The token used to signify that the incoming message is a command.</param>
         public RequestParser(string commandToken) =>
             _commandToken = commandToken;
+        /// <summary>
+        /// Attempts to parse the incoming message and generate a <see cref="CommandRequest"/>.
+        /// </summary>
+        /// <param name="message">The message to parse.</param>
+        /// <param name="request">The resulting request.</param>
+        /// <returns><see langword="true"/> if the message contains a command and is successfully parsed, otherwise <see langword="false"/>.</returns>
         public bool TryParse(string message, out CommandRequest? request)
         {
             // If the message isn't considered a command, then there's no more work to do.
