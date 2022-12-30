2c0422dd31984bd9d2c02732fe0f8c9fcd744990
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 12:44:29 -0600

    Added title to console for streaming privacy.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+Console.Title = "Freya";
 Console.WriteLine($"Initializing.");
 
 // Validate the base directory.
