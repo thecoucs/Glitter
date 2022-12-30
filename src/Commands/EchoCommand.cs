e9543d1b877bf19d9d06a1e8d0457de806511652
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 11:32:24 -0600

    Removed regions, added documentation.

--- src/Commands/EchoCommand.cs
+++ src/Commands/EchoCommand.cs
@@ -1,31 +1,29 @@
 ﻿using System.Text;
 
 using Discord.Commands;
+
 using Freya.Core;
+
 using Mauve.Runtime;
 
 namespace Freya.Commands
 {
+    /// <summary>
+    /// Represents a <see cref="Command"/> that echos input back to the user.
+    /// </summary>
     [Alias("echo")]
     internal class EchoCommand : Command
     {
-
-        #region Fields
-
         private readonly string _input;
-
-        #endregion
-
-        #region Constructor
-
+        /// <summary>
+        /// Creates a new instance of <see cref="EchoCommand"/>.
+        /// </summary>
+        /// <param name="input">The input to be echoed back to the user.</param>
+        /// <param name="logger"></param>
         public EchoCommand([FromBot] string input, ILogger<LogEntry> logger) :
             base("Echo", "Echoes the specified input back to the user.", logger) =>
             _input = input;
-
-        #endregion
-
-        #region Protected Methods
-
+        /// <inheritdoc/>
         protected override async Task Work(CancellationToken cancellationToken)
         {
             byte[] data = Encoding.UTF8.GetBytes(_input);
@@ -33,8 +31,5 @@ namespace Freya.Commands
             //await invokingService.SendResponse(response, cancellationToken);
             await Task.CompletedTask; // TODO: remove.
         }
-
-        #endregion
-
     }
 }
