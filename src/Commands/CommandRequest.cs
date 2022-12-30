81a08be05e349a8dd963515d6f29d83e1dd1fdcb
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 13:33:58 -0600

    Opted for string instead of object to reduce boxing.

--- src/Commands/CommandRequest.cs
+++ src/Commands/CommandRequest.cs
@@ -12,13 +12,13 @@
         /// <summary>
         /// The parameters provided for the command.
         /// </summary>
-        public IEnumerable<object> Parameters { get; init; }
+        public IEnumerable<string> Parameters { get; init; }
         /// <summary>
         /// Initializes a new instance of the <see cref="CommandRequest"/> class.
         /// </summary>
         /// <param name="key">The key used for invoking the command.</param>
         /// <param name="parameters">The parameters provided for the command.</param>
-        public CommandRequest(string key, params object[] parameters)
+        public CommandRequest(string key, params string[] parameters)
         {
             Key = key;
             Parameters = parameters;
