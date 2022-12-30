8df40aa5d23dfb766c1bbc50289658e91f67e2a4
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 11:09:18 -0600

    Opted for record, added documentation.

--- src/Commands/CommandRequest.cs
+++ src/Commands/CommandRequest.cs
@@ -1,8 +1,27 @@
 ﻿namespace Freya.Commands
 {
-    internal interface CommandRequest
+    /// <summary>
+    /// Represents a command request within Freya.
+    /// </summary>
+    internal record CommandRequest
     {
-        public string Key { get; set; }
-        public IEnumerable<object> Parameters { get; set; }
+        /// <summary>
+        /// The key used for invoking the command.
+        /// </summary>
+        public string Key { get; init; }
+        /// <summary>
+        /// The parameters provided for the command.
+        /// </summary>
+        public IEnumerable<object> Parameters { get; init; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandRequest"/> class.
+        /// </summary>
+        /// <param name="key">The key used for invoking the command.</param>
+        /// <param name="parameters">The parameters provided for the command.</param>
+        public CommandRequest(string key, params object[] parameters)
+        {
+            Key = key;
+            Parameters = parameters;
+        }
     }
 }
