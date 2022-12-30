b7b34ef0d7a0e77905eeb2171d472e5954923c60
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 11:09:57 -0600

    Removed regions.

--- src/Commands/CommandResponse.cs
+++ src/Commands/CommandResponse.cs
@@ -5,26 +5,15 @@
     /// </summary>
     internal sealed class CommandResponse
     {
-
-        #region Properties
-
         /// <summary>
         /// The data in the response.
         /// </summary>
         public byte[] Data { get; set; }
-
-        #endregion
-
-        #region Constructor
-
         /// <summary>
         /// Creates a new instance of <see cref="CommandResponse"/>.
         /// </summary>
         /// <param name="data">The data in the response.</param>
         public CommandResponse(byte[] data) =>
             Data = data;
-
-        #endregion
-
     }
 }
