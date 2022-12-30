d9d4c142237b073d530f907068f6de6a6a447aae
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 21:04:29 -0600

    Moved alias factory logic to command factory.

--- src/Runtime/CommandFactory.cs
+++ src/Runtime/CommandFactory.cs
@@ -2,6 +2,62 @@
 {
     internal class CommandFactory
     {
+        //protected Type? GetTypeForAlias(string alias)
+        //{
+        //    // If we already have the requested alias, return the associated type.
+        //    if (_types?.ContainsKey(alias) == true)
+        //        return _types[alias];
 
+        //    // If the alias wasn't found but we've already recorded all eligible types, then return null.
+        //    else if (_types is not null)
+        //        return null;
+
+        //    // Get the entry assembly, if we can't find it, there's no work.
+        //    var assembly = Assembly.GetEntryAssembly();
+        //    if (assembly is null)
+        //        return null;
+
+        //    // Get any types considered valid for the factory.
+        //    IEnumerable<Type> types = from type
+        //                              in assembly.GetTypes()
+        //                              where !type.IsAbstract &&
+        //                                    !type.IsInterface &&
+        //                                    type.GetCustomAttribute<AliasAttribute>() is not null &&
+        //                                    type.DerivesFrom<T>()
+        //                              select type;
+
+        //    // Store the types with their associated aliases.
+        //    _types = new Dictionary<string, Type>();
+        //    foreach (Type type in types)
+        //    {
+        //        // Get the type alias and validate it.
+        //        AliasAttribute? typeAlias = type?.GetCustomAttribute<AliasAttribute>();
+        //        if (typeAlias is null)
+        //            continue;
+
+        //        // Iterate over the identified aliases, validating, and then recording them.
+        //        foreach (string value in typeAlias.Values)
+        //        {
+        //            // Validate the value.
+        //            if (string.IsNullOrWhiteSpace(value))
+        //                continue;
+
+        //            // Prevent duplicate associations.
+        //            if (_types?.ContainsKey(value) == true)
+        //                throw new InvalidOperationException("Unable to register more than one type with a single alias.");
+
+        //            // Record the association.
+        //            if (_types is not null && type is not null)
+        //                _types.Add(value, type);
+        //        }
+        //    }
+
+        //    // If we already have the requested alias, return the associated type.
+        //    if (_types?.ContainsKey(alias) == true)
+        //        return _types[alias];
+
+        //    // If we haven't found the requested alias by now, we're not going to.
+        //    return null;
+        //}
     }
 }
