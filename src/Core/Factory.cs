29269c5935fabcf995818c6b31bf8db6be0e25ea
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 19:12:44 -0600

    Created basic factory logic.

--- src/Core/Factory.cs
+++ src/Core/Factory.cs
@@ -1,6 +1,50 @@
-﻿namespace Freya.Core
+﻿using Mauve;
+using Mauve.Extensibility;
+using Mauve.Runtime;
+
+namespace Freya.Core
 {
-    internal class Factory
+    internal abstract class Factory<T>
     {
+
+        #region Fields
+
+        private readonly ILogger<LogEntry> _logger;
+
+        #endregion
+
+        #region Constructor
+
+        public Factory(ILogger<LogEntry> logger) =>
+            _logger = logger;
+
+        #endregion
+
+        #region Public Methods
+
+        public bool TryCreate(string alias, out T? instance)
+        {
+            T? result = default;
+            try
+            {
+                result = CreateInstance(alias);
+            } catch (Exception e)
+            {
+                string message = $"An unexpected error occurred while creating an instance of {typeof(T).Name} with alias {alias}. {e.FlattenMessages()}";
+                _logger.Log(new LogEntry(EventType.Exception, message));
+            }
+
+            instance = result;
+            return instance is not null;
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected abstract T CreateInstance(string alias);
+
+        #endregion
+
     }
 }
