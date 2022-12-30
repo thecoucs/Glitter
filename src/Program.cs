17cf66db937eb38bcc6558f08dda678b55455f24
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 21:05:55 -0600

    Added MSDI.

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,11 @@
-﻿using Freya;
+﻿using Freya.Core;
+using Freya.Runtime;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine($"Starting Freya.");
+Console.WriteLine($"Initializing.");
 
 // Validate the base directory.
 string baseDirectory = AppContext.BaseDirectory;
@@ -21,13 +24,46 @@ if (parentDirectory is null)
 }
 
 // Build configuration
+Console.WriteLine("Building configuration.");
 IConfiguration configuration = new ConfigurationBuilder()
     .SetBasePath(parentDirectory.FullName)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile("appsettings.development.json", optional: true)
     .Build();
 
-// Create the brain and wake it up.
-var brain = new Brain();
-brain.Configure(configuration);
+// Create a new brain and host.
+Brain brain = null;
+Console.WriteLine("Creating host.");
+using IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        // Register the factories.
+        Console.WriteLine("Registering factories.");
+        var commandFactory = new CommandFactory();
+        var serviceFactory = new ServiceFactory(configuration, commandFactory);
+        _ = services.AddSingleton(serviceFactory)
+                    .AddSingleton(commandFactory);
+
+        // Configure the brain.
+        Console.WriteLine("Configuring.");
+        brain = new Brain(serviceFactory);
+        brain.Configure(services);
+    })
+    .Build();
+
+// Validate the brain.
+if (brain is null)
+{
+    Console.WriteLine("Unable to start. The brain does not exist.");
+    return;
+}
+
+// Signal that initialization is complete.
+ConsoleColor initialColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Initialization complete.");
+Console.ForegroundColor = initialColor;
+
+// Wake the brain up.
+Console.WriteLine("Starting services.");
 await brain.Start();
