34d607912d1cd5344f4d5c1b047261425d37b945
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-29 21:03:51 -0600

    Created aliased type factory.

---
+++ src/Core/AliasedTypeFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+using Mauve;
+using Mauve.Extensibility;
+
+namespace Freya.Core
+{
+    internal abstract class AliasedTypeFactory<T>
+    {
+
+        #region Protected Methods
+
+        protected IEnumerable<Type>? GetQualifiedTypes()
+        {
+            // Get the entry assembly, if we can't find it, there's no work.
+            var assembly = Assembly.GetEntryAssembly();
+            if (assembly is null)
+                return null;
+
+            // Get any types considered valid for the factory.
+            return from type
+                   in assembly.GetTypes()
+                   where !type.IsAbstract &&
+                         !type.IsInterface &&
+                         type.GetCustomAttribute<AliasAttribute>() is not null &&
+                         type.DerivesFrom<T>()
+                   select type;
+        }
+
+        #endregion
+
+    }
+}
