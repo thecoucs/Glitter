7d94e25870a8e84eabc8a5c589c839d68d5101d3
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 12:42:50 -0600

    Updated documentation.

--- src/Services/TestService.cs
+++ src/Services/TestService.cs
@@ -8,24 +8,21 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Freya.Services
 {
+    /// <summary>
+    /// Represents a test service for testing within the deployed console.
+    /// </summary>
     [Alias("test")]
     internal class TestService : BotService
     {
-
-        #region Constructor
-
         /// <summary>
-        /// 
+        /// Creates a new <see cref="TestService"/> instance.
         /// </summary>
+        /// <param name="commandFactory">The command factory for the service to create commands.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
         public TestService(CommandFactory commandFactory, CancellationToken cancellationToken) :
             base("Test", new ConsoleLogger(), commandFactory, cancellationToken)
         { }
-
-        #endregion
-
-        #region Protected Methods
-
+        /// <inheritdoc/>
         protected override void ConfigureService(IServiceCollection services, IPipeline<Command> pipeline)
         {
 
@@ -36,8 +33,5 @@ namespace Freya.Services
             cancellationToken.ThrowIfCancellationRequested();
             await Log(EventType.Information, "The test service is alive.");
         }
-
-        #endregion
-
     }
 }
