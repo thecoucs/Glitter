ed50499f7188790a318460db9b76a5da0a3fafe1
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 18:19:57 -0600

    Cleaned up launch logic.

--- src/Program.cs
+++ src/Program.cs
@@ -1,64 +1,33 @@
-﻿using Freya.Commands;
-using Freya.Core;
-using Freya.Services;
+﻿using System.Reflection;
 
-using Microsoft.Extensions.Configuration;
+using Freya.Core;
+using Freya.Runtime;
+
+using Mauve.Runtime;
+
+using MediatR;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 Console.Title = "Freya";
 Console.WriteLine($"Initializing.");
 
-// Validate the base directory.
-string baseDirectory = AppContext.BaseDirectory;
-if (string.IsNullOrWhiteSpace(baseDirectory))
-{
-    Console.WriteLine("Unable to start. The base directory is null or empty.");
-    return;
-}
-
-// Validate the parent directory.
-DirectoryInfo? parentDirectory = Directory.GetParent(baseDirectory);
-if (parentDirectory is null)
-{
-    Console.WriteLine("Unable to start. The parent directory is null.");
-    return;
-}
-
-// Build configuration
-Console.WriteLine("Building configuration.");
-IConfiguration configuration = new ConfigurationBuilder()
-    .SetBasePath(parentDirectory.FullName)
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile("appsettings.development.json", optional: true)
-    .Build();
-
 // Create a new brain and host.
-Brain? brain = null;
 Console.WriteLine("Creating host.");
 using IHost host = Host.CreateDefaultBuilder()
     .ConfigureServices(services =>
     {
-        // Register the factories.
-        Console.WriteLine("Registering factories.");
-        var commandFactory = new CommandFactory();
-        var serviceFactory = new ServiceFactory(configuration, commandFactory);
-        _ = services.AddSingleton(serviceFactory)
-                    .AddSingleton(commandFactory);
+        // Add services.
+        _ = services.AddMediatR(Assembly.GetExecutingAssembly())
+                    .AddSingleton(new RequestParser("!"))
+                    .AddSingleton<ILogger<LogEntry>>(new ConsoleLogger());
 
-        // Configure the brain.
-        Console.WriteLine("Configuring.");
-        brain = new Brain(serviceFactory);
-        brain.Configure(services);
+        // Create a provider and register it for the command factory.
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        _ = services.AddSingleton(serviceProvider);
     })
     .Build();
-
-// Validate the brain.
-if (brain is null)
-{
-    Console.WriteLine("Unable to start. The brain does not exist.");
-    return;
-}
 
 // Signal that initialization is complete.
 ConsoleColor initialColor = Console.ForegroundColor;
@@ -68,4 +37,5 @@ Console.ForegroundColor = initialColor;
 
 // Wake the brain up.
 Console.WriteLine("Starting services.");
+var brain = new Brain(host.Services);
 await brain.Start();
