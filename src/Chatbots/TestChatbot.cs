8b89d922583799c13978bb6468cb989477e91004
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-30 18:34:23 -0600

    Renamed test chatbot.

--- src/Chatbots/TestChatbot.cs
+++ src/Chatbots/TestChatbot.cs
@@ -11,14 +11,14 @@ namespace Freya.Services
     /// Represents a test service for testing within the deployed console.
     /// </summary>
     [Alias("test")]
-    internal class TestService : Chatbot
+    internal class TestChatbot : Chatbot
     {
         /// <summary>
-        /// Creates a new <see cref="TestService"/> instance.
+        /// Creates a new <see cref="TestChatbot"/> instance.
         /// </summary>
         /// <param name="commandFactory">The command factory for the service to create commands.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
-        public TestService(ILogger<LogEntry> logger, IMediator mediator, CancellationToken cancellationToken) :
+        public TestChatbot(ILogger<LogEntry> logger, IMediator mediator, CancellationToken cancellationToken) :
             base("Test", logger, mediator, cancellationToken)
         { }
         /// <inheritdoc/>
