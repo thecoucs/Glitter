185d15f7c30c7190812df1f981248fe25d993ec3
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2022-12-31 18:24:56 -0600

    Created Discord user adapter shell.

---
+++ src/Providers/Discord/Adapters/DiscordUserAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freya.Providers.Discord.Adapters
+{
+    internal class DiscordUserAdapter
+    {
+    }
+}
