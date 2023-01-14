f8571e744fea86bf1e6fee09829e26bbb0657671
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 10:46:33 -0600

    Sealed logged in event handler.

--- src/Freya.Providers.Discord/Events/LoggedInEventHandler.cs
+++ src/Freya.Providers.Discord/Events/LoggedInEventHandler.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Discord.WebSocket;
+
 using Microsoft.Extensions.Logging;
 
 namespace Freya.Providers.Discord.Events
@@ -10,7 +7,7 @@ namespace Freya.Providers.Discord.Events
     /// <summary>
     /// Represents an <see cref="EventHandler"/> for handling the LoggedIn event for a <see cref="DiscordSocketClient"/>.
     /// </summary>
-    internal class LoggedInEventHandler : EventHandler
+    internal sealed class LoggedInEventHandler : EventHandler
     {
         /// <summary>
         /// Creates a new <see cref="LoggedInEventHandler"/> instance.
