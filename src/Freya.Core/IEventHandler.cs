7f13f29bf71909be4f673b6ae982e0c1af09b850
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 09:45:19 -0600

    Created event handler interface.

---
+++ src/Freya.Core/IEventHandler.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freya
+{
+    /// <summary>
+    /// A simple marker interface to wake up event handler implementations.
+    /// </summary>
+    /// TODO: Redesign event handling so that this interface isn't needed.
+    public interface IEventHandler
+    {
+        
+    }
+}
