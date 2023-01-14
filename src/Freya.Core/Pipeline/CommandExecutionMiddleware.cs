b38760856b1f596f377b36b906be26c124d20aaa
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 00:12:22 -0600

    Removed local cancellation token.

--- src/Freya.Core/Pipeline/CommandExecutionMiddleware.cs
+++ src/Freya.Core/Pipeline/CommandExecutionMiddleware.cs
@@ -10,15 +10,9 @@ namespace Freya.Pipeline
     /// <inheritdoc/>
     public class CommandExecutionMiddleware : IMiddleware<Command>
     {
-        private readonly CancellationToken _cancellationToken;
-        /// <summary>
-        /// Creates a new instance of <see cref="CommandExecutionMiddleware"/>.
-        /// </summary>
-        public CommandExecutionMiddleware(CancellationToken cancellationToken) =>
-            _cancellationToken = cancellationToken;
         /// <inheritdoc/>
         public async Task Invoke(Command input, IMiddleware<Command> next) =>
-            _ = await input.Execute(_cancellationToken);
+            _ = await input.Execute(CancellationToken.None);
         /// <inheritdoc/>
         public async Task Invoke(Command input, IMiddleware<Command> next, CancellationToken cancellationToken)
         {
