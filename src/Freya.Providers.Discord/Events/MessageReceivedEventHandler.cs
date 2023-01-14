5e9c4de3d88a7bec2706cc048bf21b5e9958e91a
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 10:43:51 -0600

    Added message received event handler.

---
+++ src/Freya.Providers.Discord/Events/MessageReceivedEventHandler.cs
@@ -0,0 +1,26 @@
+using Discord.WebSocket;
+
+using Microsoft.Extensions.Logging;
+
+namespace Freya.Providers.Discord.Events
+{
+    /// <summary>
+    /// Represents an <see cref="EventHandler"/> for handling the connected event for a <see cref="DiscordSocketClient"/>.
+    /// </summary>
+    internal sealed class MessageReceivedEventHandler : EventHandler
+    {
+        /// <summary>
+        /// Creates a new <see cref="MessageReceivedEventHandler"/> instance.
+        /// </summary>
+        /// <param name="client">The <see cref="DiscordSocketClient"/> to handle connected events for.</param>
+        /// <param name="logger">The logger for the <see cref="DiscordChatbot"/>.</param>
+        public MessageReceivedEventHandler(DiscordSocketClient client, ILogger<DiscordChatbot> logger) :
+            base(logger) =>
+            client.Connected += HandleConnected;
+        private async Task HandleConnected()
+        {
+            Logger.LogInformation("Successfully connected to Discord.");
+            await Task.CompletedTask;
+        }
+    }
+}
