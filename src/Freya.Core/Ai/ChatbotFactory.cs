8d0cf9ea2089c8762e1240fb0102479ee3e0c047
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 00:16:41 -0600

    Simplified chatbot factory.

--- src/Freya.Core/Ai/ChatbotFactory.cs
+++ src/Freya.Core/Ai/ChatbotFactory.cs
@@ -34,82 +34,17 @@ namespace Freya.Services
         /// <returns>An <see cref="IEnumerable{T}"/> containing all concrete <see cref="Chatbot"/> implementations in the current <see cref="Assembly"/>.</returns>
         public IEnumerable<Chatbot> Discover(CancellationToken cancellationToken)
         {
-            // Get any types considered valid for the factory.
-            IEnumerable<Type>? types = GetQualifiedTypes();
-            if (types is null)
-                yield break;
-
-            // Attempt to create an instance of each type.
-            foreach (Type type in types)
+            try
             {
-                // Currently, an alias is required for settings.
-                if (!TryGetSettings(type, out object? settings))
-                    continue;
-
-                // Validate the type.
-                if (type is null)
-                    continue;
-
-                // Create an instance of the service.
-                object? instance = settings is null
-                    ? ActivatorUtilities.CreateInstance(_serviceProvider, type, cancellationToken)
-                    : ActivatorUtilities.CreateInstance(_serviceProvider, type, settings, cancellationToken);
-
-                // Validate the instance and register it.
-                if (instance is Chatbot botService)
-                    yield return botService;
+                cancellationToken.ThrowIfCancellationRequested();
+                return _serviceProvider.GetServices<Chatbot>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
-            // Signal to consumers that we're done iterating.
-            yield break;
-        }
-        /// <summary>
-        /// Attempts to load settings for the specified type.
-        /// </summary>
-        /// <param name="type">The type to load settings for.</param>
-        /// <param name="settings">The resulting settings loaded from the application configuration.</param>
-        /// <returns><see langword="true"/> if settings are not applicable or if they were successfully loaded, otherwise <see langword="false"/>.</returns>
-        private bool TryGetSettings(Type type, out object? settings)
-        {
-            // Set settings to null by default and validate the input.
-            settings = null;
-            if (type is null)
-                return false;
-
-            // Bots with settings specify a type.
-            Type[]? genericArguments = type?.BaseType?.GetGenericArguments();
-            if (genericArguments?.Length == 0)
-                return true;
-            else if (genericArguments?.Length != 1)
-                return false;
-
-            AliasAttribute? alias = type?.GetCustomAttribute<AliasAttribute>();
-            if (alias is not null)
-            {
-                // If there's more than one alias, we don't know which one to use.
-                if (alias.Values.Count > 1)
-                    return false;
-
-                // Get the configuration section.
-                string? key = alias.Values.FirstOrDefault();
-                if (!string.IsNullOrWhiteSpace(key))
-                {
-                    // Attempt to get a config section for the alias.
-                    IConfigurationSection configurationSection = _configuration.GetSection(key);
-                    if (configurationSection is null)
-                        return false;
-
-                    // Get the setting type, if one is not present, there's no work.
-                    Type? settingType = genericArguments.FirstOrDefault();
-                    if (settingType is null)
-                        return false;
-
-                    // Attempt to parse the settings.
-                    settings = configurationSection.Get(settingType);
-                }
-            }
-
-            return true;
+            return Enumerable.Empty<Chatbot>();
         }
     }
 }
