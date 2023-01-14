984f6932660e82e6bd53a264f52c574cf48bbb7f
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 00:40:22 -0600

    Resolved warnings in Discord bot.

--- src/Freya.Providers.Discord/DiscordChatbot.cs
+++ src/Freya.Providers.Discord/DiscordChatbot.cs
@@ -51,7 +51,8 @@ namespace Freya.Providers.Discord
                 await _client.LoginAsync(TokenType.Bot, Settings.Token);
                 await _client.StartAsync();
                 Log(LogLevel.Information, "The Discord service has been started successfully.");
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Log(LogLevel.Information, $"An unexpected error occurred while starting the Discord service. {e.Message}");
             }
@@ -59,14 +60,26 @@ namespace Freya.Providers.Discord
         private Task HandleClientInviteCreation(SocketInvite arg) => throw new NotImplementedException();
         private Task HandleClientScheduledGuildEventCreation(SocketGuildEvent arg) => throw new NotImplementedException();
         private Task HandleClientGuildJoin(SocketGuild arg) => throw new NotImplementedException();
-        private async Task HandleClientConnect() =>
+        private async Task HandleClientConnect()
+        {
             Log(LogLevel.Information, "Successfully connected to Discord.");
-        private async Task HandleClientDisconnect(Exception arg) =>
+            await Task.CompletedTask;
+        }
+        private async Task HandleClientDisconnect(Exception arg)
+        {
             Log(LogLevel.Error, $"Disconnected from Discord. {arg.FlattenMessages(" ")}");
-        private async Task HandleClientLogin() =>
+            await Task.CompletedTask;
+        }
+        private async Task HandleClientLogin()
+        {
             Log(LogLevel.Information, "Logged in to Discord.");
-        private async Task HandleClientLogout() =>
+            await Task.CompletedTask;
+        }
+        private async Task HandleClientLogout()
+        {
             Log(LogLevel.Warning, "Logged out of Discord.");
+            await Task.CompletedTask;
+        }
         private async Task HandleClientLog(LogMessage arg)
         {
             // Determine the event type.
@@ -84,11 +97,15 @@ namespace Freya.Providers.Discord
                 try
                 {
                     Log(LogLevel.Error, arg.Exception.FlattenMessages(" "));
-                } catch (Exception e)
+                }
+                catch (Exception e)
                 {
                     Log(LogLevel.Error, $"An unexpected error occurred while recording a log from Discord. {e.Message}");
+                    await Task.FromException(e);
                 }
             }
+
+            await Task.CompletedTask;
         }
         private async Task HandleClientMessage(SocketMessage arg) =>
             await Task.CompletedTask;
