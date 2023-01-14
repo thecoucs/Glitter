c769cfa44521e9f53e3bcd1c38ee500825a80f9e
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 00:15:01 -0600

    Removed await from log invocations.

--- src/Freya.Providers.Discord/DiscordChatbot.cs
+++ src/Freya.Providers.Discord/DiscordChatbot.cs
@@ -23,14 +23,12 @@ namespace Freya.Providers.Discord
         /// <summary>
         /// Creates a new <see cref="DiscordChatbot"/> instance.
         /// </summary>
-        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
         public DiscordChatbot(
             DiscordSettings settings,
             RequestParser parser,
             ILogger<DiscordChatbot> logger,
-            IMediator mediator,
-            CancellationToken cancellationToken) :
-            base("Discord", settings, parser, logger, mediator, cancellationToken) =>
+            IMediator mediator) :
+            base("Discord", settings, parser, logger, mediator) =>
             _client = new DiscordSocketClient();
         /// <inheritdoc/>
         protected override void Initialize()
@@ -53,23 +51,23 @@ namespace Freya.Providers.Discord
             {
                 await _client.LoginAsync(TokenType.Bot, Settings.Token);
                 await _client.StartAsync();
-                await Log(LogLevel.Information, "The Discord service has been started successfully.");
+                Log(LogLevel.Information, "The Discord service has been started successfully.");
             } catch (Exception e)
             {
-                await Log(LogLevel.Information, $"An unexpected error occurred while starting the Discord service. {e.Message}");
+                Log(LogLevel.Information, $"An unexpected error occurred while starting the Discord service. {e.Message}");
             }
         }
         private Task HandleClientInviteCreation(SocketInvite arg) => throw new NotImplementedException();
         private Task HandleClientScheduledGuildEventCreation(SocketGuildEvent arg) => throw new NotImplementedException();
         private Task HandleClientGuildJoin(SocketGuild arg) => throw new NotImplementedException();
         private async Task HandleClientConnect() =>
-            await Log(LogLevel.Information, "Successfully connected to Discord.");
+            Log(LogLevel.Information, "Successfully connected to Discord.");
         private async Task HandleClientDisconnect(Exception arg) =>
-            await Log(LogLevel.Error, $"Disconnected from Discord. {arg.FlattenMessages(" ")}");
+            Log(LogLevel.Error, $"Disconnected from Discord. {arg.FlattenMessages(" ")}");
         private async Task HandleClientLogin() =>
-            await Log(LogLevel.Information, "Logged in to Discord.");
+            Log(LogLevel.Information, "Logged in to Discord.");
         private async Task HandleClientLogout() =>
-            await Log(LogLevel.Warning, "Logged out of Discord.");
+            Log(LogLevel.Warning, "Logged out of Discord.");
         private async Task HandleClientLog(LogMessage arg)
         {
             // Determine the event type.
@@ -79,17 +77,17 @@ namespace Freya.Providers.Discord
 
             // Log the original message.
             if (!string.IsNullOrWhiteSpace(arg.Message))
-                await Log(logLevel, arg.Message);
+                Log(logLevel, arg.Message);
 
             // Log the exception separately.
             if (arg.Exception is not null)
             {
                 try
                 {
-                    await Log(LogLevel.Error, arg.Exception.FlattenMessages(" "));
+                    Log(LogLevel.Error, arg.Exception.FlattenMessages(" "));
                 } catch (Exception e)
                 {
-                    await Log(LogLevel.Error, $"An unexpected error occurred while recording a log from Discord. {e.Message}");
+                    Log(LogLevel.Error, $"An unexpected error occurred while recording a log from Discord. {e.Message}");
                 }
             }
         }
