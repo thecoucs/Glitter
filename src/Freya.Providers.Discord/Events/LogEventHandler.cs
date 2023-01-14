cae371e86dc7957897f925ce03846f113cbb4853
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 10:10:47 -0600

    Updated XML documentation.

--- src/Freya.Providers.Discord/Events/LogEventHandler.cs
+++ src/Freya.Providers.Discord/Events/LogEventHandler.cs
@@ -10,9 +10,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Freya.Providers.Discord.Events
 {
+    /// <summary>
+    /// Represents an <see cref="IEventHandler"/> for handling the Log event for a <see cref="DiscordSocketClient"/>.
+    /// </summary>
     internal sealed class LogEventHandler : IEventHandler
     {
         private readonly ILogger _logger;
+        /// <summary>
+        /// Creates a new <see cref="LogEventHandler"/> instance.
+        /// </summary>
+        /// <param name="client">The <see cref="DiscordSocketClient"/> to handle log events for.</param>
+        /// <param name="logger">The logger for the <see cref="DiscordChatbot"/>.</param>
         public LogEventHandler(DiscordSocketClient discordClient, ILogger<DiscordChatbot> logger) {
             _logger = logger;
             discordClient.Log += HandleLogMessage;
