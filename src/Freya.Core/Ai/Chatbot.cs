22eeab59f778e816e4a7102d14c8b5c9fdecc7bd
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-14 00:11:38 -0600

    Removed cancellation token, removed async from Log.

--- src/Freya.Core/Ai/Chatbot.cs
+++ src/Freya.Core/Ai/Chatbot.cs
@@ -16,27 +16,23 @@ namespace Freya.Services
     /// </summary>
     public abstract class Chatbot
     {
-        private readonly string _name;
         private readonly ILogger _logger;
-        private readonly CancellationToken _cancellationToken;
         protected RequestParser Parser { get; set; }
         protected IMediator Mediator { get; set; }
+        public string Name { get; set; }
         /// <summary>
         /// Creates a new <see cref="Chatbot"/> instance.
         /// </summary>
         /// <param name="name">The name of the service.</param>
         /// <param name="logger">The logger to be utilized by the service.</param>
-        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to be utilized during execution to signal cancellation.</param>
         public Chatbot(
             string name,
             RequestParser parser,
             ILogger logger,
-            IMediator mediator,
-            CancellationToken cancellationToken)
+            IMediator mediator)
         {
-            _name = name;
+            Name = name;
             _logger = logger;
-            _cancellationToken = cancellationToken;
             Parser = parser;
             Mediator = mediator;
         }
@@ -48,30 +44,30 @@ namespace Freya.Services
         public virtual void Configure(IPipeline<Command> pipeline)
         {
             // Add the basic execution pipeline.
-            var executor = new CommandExecutionMiddleware(_cancellationToken);
+            var executor = new CommandExecutionMiddleware();
             pipeline.Run(executor);
         }
         /// <summary>
         /// Starts the <see cref="Chatbot"/>.
         /// </summary>
         /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
-        public async Task Start()
+        public async Task Start(CancellationToken cancellationToken)
         {
             // Cancel if requested, otherwise create the command pipeline and dependency collection.
-            _cancellationToken.ThrowIfCancellationRequested();
-            await Log(LogLevel.Information, "Initializing.");
+            cancellationToken.ThrowIfCancellationRequested();
+            Log(LogLevel.Information, "Initializing.");
             Initialize();
 
             // Cancel if requested, otherwise run the service.
-            await Log(LogLevel.Information, $"Starting.");
-            _cancellationToken.ThrowIfCancellationRequested();
+            Log(LogLevel.Information, $"Starting.");
+            cancellationToken.ThrowIfCancellationRequested();
             _ = Task.Run(async () =>
             {
-                await Run(_cancellationToken);
+                await Run(cancellationToken);
 
                 // Keep the bot alive.
                 await Task.Delay(Timeout.Infinite);
-            }, _cancellationToken);
+            }, cancellationToken);
             await Task.CompletedTask;
         }
         protected virtual void Initialize() { }
@@ -87,11 +83,7 @@ namespace Freya.Services
         /// <param name="eventType">The <see cref="EventType"/> associated with the message.</param>
         /// <param name="message">The message to log.</param>
         /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
-        protected async Task Log(LogLevel level, string message)
-        {
-            _cancellationToken.ThrowIfCancellationRequested();
-            _logger.Log(level, $"{_name}: {message}");
-            await Task.CompletedTask;
-        }
+        protected void Log(LogLevel level, string message) =>
+            _logger.Log(level, $"{Name}: {message}");
     }
 }
