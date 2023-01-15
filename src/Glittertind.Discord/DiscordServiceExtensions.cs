8ebdd90e3ad29d7fc6b731926674d2b8b32ef373
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 13:04:30 -0600

    Updated extension methods to reflect README instructions.

--- src/Glittertind.Discord/DiscordServiceExtensions.cs
+++ src/Glittertind.Discord/DiscordServiceExtensions.cs
@@ -6,23 +6,52 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Glittertind.Discord
 {
+    /// <summary>
+    /// Represents a collection of extension methods for adding Discord support to a chatbot application.
+    /// </summary>
     public static class DiscordServiceExtensions
     {
         /// <summary>
-        /// Adds the <see cref="DiscordChatbot"/> to the DI container.
+        /// Adds support for Discord to the DI container.
         /// </summary>
-        /// <param name="services">The service contract for adding services to the DI container.</param>
-        /// <returns>The current <see cref="IServiceCollection"/> instance containing <see cref="DiscordChatbot"/> as a singleton service.</returns>
-        public static SynapseBuilder AddDiscord(this SynapseBuilder registrar) =>
-            registrar.AddChatbot<DiscordChatbot>()
-                     .AddSettings<DiscordSettings>("discord")
-                     .AddEventHandler<LogEventHandler>()
-                     .AddEventHandler<LogEventHandler>()
-                     .AddEventHandler<LoggedInEventHandler>()
-                     .AddEventHandler<LoggedOutEventHandler>()
-                     .AddEventHandler<ConnectedEventHandler>()
-                     .AddEventHandler<DisconnectedEventHandler>()
-                     .AddEventHandler<MessageReceivedEventHandler>()
-                     .AddServices(services => services.AddSingleton<DiscordSocketClient>());
+        /// <param name="synapses">The <see cref="SynapseBuilder"/> to be used for adding Discord to the service contract.</param>
+        /// <returns>The current <see cref="SynapseBuilder"/> instance containing <see cref="DiscordChatbot"/> and its dependencies as a service.</returns>
+        public static SynapseBuilder AddDiscord(this SynapseBuilder synapses) =>
+            RegisterDiscordServices(synapses, settings: null);
+        /// <summary>
+        /// Adds support for Discord to the DI container.
+        /// </summary>
+        /// <param name="synapses">The <see cref="SynapseBuilder"/> to be used for adding Discord to the service contract.</param>
+        /// <param name="token">The authentication token that should be used to communicate with Discord.</param>
+        /// <returns>The current <see cref="SynapseBuilder"/> instance containing <see cref="DiscordChatbot"/> and its dependencies as a service.</returns>
+        public static SynapseBuilder AddDiscord(this SynapseBuilder synapses, string token)
+        {
+            // Create a new set of settings for Discord.
+            var settings = new DiscordSettings
+            {
+                Token = token
+            };
+
+            // Register services and return the builder.
+            return RegisterDiscordServices(synapses, settings);
+        }
+        private static SynapseBuilder RegisterDiscordServices(this SynapseBuilder synapses, DiscordSettings? settings)
+        {
+            // Determine if settings should be loaded or built.
+            _ = settings is null
+                ? synapses.AddSettings<DiscordSettings>("Discord")
+                : synapses.AddServices(services => services.AddSingleton(settings));
+
+            // Register remaining services and return the builder.
+            return synapses.AddChatbot<DiscordChatbot>()
+                           .AddEventHandler<LogEventHandler>()
+                           .AddEventHandler<LogEventHandler>()
+                           .AddEventHandler<LoggedInEventHandler>()
+                           .AddEventHandler<LoggedOutEventHandler>()
+                           .AddEventHandler<ConnectedEventHandler>()
+                           .AddEventHandler<DisconnectedEventHandler>()
+                           .AddEventHandler<MessageReceivedEventHandler>()
+                           .AddServices(services => services.AddSingleton<DiscordSocketClient>());
+        }
     }
 }
