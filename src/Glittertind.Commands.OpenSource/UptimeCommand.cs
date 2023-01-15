4508fde205bc2575435b9dd4b407858f7692ec98
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 13:34:26 -0600

    Created uptime command and request.

---
+++ src/Glittertind.Commands.OpenSource/UptimeCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace Glittertind.Commands.OpenSource
+{
+    /// <summary>
+    /// Represents a <see cref="Command"/> for getting the uptime of the bot.
+    /// </summary>
+    public sealed class UptimeCommand : Command
+    {
+        /// <summary>
+        /// Creates a new <see cref="UptimeCommand"/> instance.
+        /// </summary>
+        /// <param name="logger">The logger the command should use.</param>
+        public UptimeCommand(IMediator mediator, ILogger<UptimeCommand> logger) :
+            base("uptime", "Get Uptime", "Queries the amount of time the bot has been up and running.", mediator, logger)
+        { }
+        protected override async Task<CommandResponse> Work(CancellationToken cancellationToken)
+        {
+            var request = new UptimeRequest();
+            TimeSpan uptime = await Mediator.Send(request);
+            return new CommandResponse($"The current uptime is {uptime}.");
+        }
+    }
+}
---
+++ src/Glittertind.Commands.OpenSource/UptimeRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Glittertind.Commands.OpenSource
+{
+    /// <summary>
+    /// Represents a request for the uptime of the bot.
+    /// </summary>
+    public class UptimeRequest : IRequest<TimeSpan>
+    {
+
+    }
+}
