90e44e2fdb29ba9a1ff94ef67236361fb885ea50
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 13:21:14 -0600

    Added key property and constructor.

--- src/Glittertind/SystemCommandAttribute.cs
+++ src/Glittertind/SystemCommandAttribute.cs
@@ -6,5 +6,15 @@
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class SystemCommandAttribute : Attribute
     {
+        /// <summary>
+        /// The key utilized by consumers to invoke the command.
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// Creates a new <see cref="SystemCommandAttribute"/> instance.
+        /// </summary>
+        /// <param name="key">The key utilized by consumers to invoke the command.</param>
+        public SystemCommandAttribute(string key) =>
+            Key = key;
     }
 }
