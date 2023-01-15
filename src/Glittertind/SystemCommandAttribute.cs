1a39d80d429a9df090bf44742b63cade711c7276
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 13:18:49 -0600

    Created system command attribute.

---
+++ src/Glittertind/SystemCommandAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Glittertind
+{
+    /// <summary>
+    /// Represents an <see cref="Attribute"/> for marking a <see cref="Command"/> as a system command.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class SystemCommandAttribute : Attribute
+    {
+    }
+}
