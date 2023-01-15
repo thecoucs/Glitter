bf24e3fcd7a9f39a053e4b2ad99dd305d388fd41
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 14:30:24 -0600

    Removed from bot attribute.

--- src/Glittertind/FromBotAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Glittertind
-{
-    /// <summary>
-    /// Allows specification that a member comes from the bot.
-    /// </summary>
-    public class FromBotAttribute : Attribute
-    {
-    }
-}
