0dadae0811b65f4fc131e95dc6025a39fc29e0a8
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 22:37:24 -0600

    Added XML documentation to slash command.

--- src/Glitter/Commands/SlashCommand.cs
+++ src/Glitter/Commands/SlashCommand.cs
@@ -5,10 +5,16 @@ namespace Glitter.Commands;
 /// <summary>
 /// Represents a slash command that can be executed by a chatbot.
 /// </summary>
-public class SlashCommand : Command
+public abstract class SlashCommand : Command
 {
+    /// <summary>
+    /// Creates a new <see cref="SlashCommand"/> instance.
+    /// </summary>
+    /// <param name="key">The key for invoking the command.</param>
+    /// <param name="displayName">The display name for the command.</param>
+    /// <param name="description">The description for the command.</param>
+    /// <param name="logger">The logger the command should utilize.</param>
     public SlashCommand(string key, string displayName, string description, ILogger logger) :
         base(key, displayName, description, logger)
     { }
-    protected override Task<CommandResponse> Work(CancellationToken cancellationToken) => throw new NotImplementedException();
 }
