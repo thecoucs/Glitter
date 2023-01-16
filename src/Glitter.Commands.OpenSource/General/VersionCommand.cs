ed7cee9eb6c55b6bb6c2e3206e0518f83f2891f7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 20:56:09 -0600

    Added version command.

---
+++ src/Glitter.Commands.OpenSource/General/VersionCommand.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Glitter.Commands.OpenSource.General;
+
+/// <summary>
+/// Represents a <see cref="Command"/> for getting the current version of the bot.
+/// </summary>
+public sealed class VersionCommand : Command
+{
+    /// <summary>
+    /// Creates a new <see cref="VersionCommand"/> instance.
+    /// </summary>
+    /// <param name="logger">The logger for the command to use.</param>
+    public VersionCommand(ILogger<VersionCommand> logger) :
+        base("version", "Version", "Gets the current version of the bot.", logger)
+    { }
+    protected override async Task<CommandResponse> Work(CancellationToken cancellationToken)
+    {
+        var response = new CommandResponse($"🐣 Pre-Release");
+        return await Task.FromResult(response);
+    }
+}
