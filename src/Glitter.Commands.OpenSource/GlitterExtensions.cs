7b61c9caf6056c57654a53c36e9e4df3c25b4ddc
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 20:56:19 -0600

    Updated extension method.

--- src/Glitter.Commands.OpenSource/GlitterExtensions.cs
+++ src/Glitter.Commands.OpenSource/GlitterExtensions.cs
@@ -1,23 +1,27 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Glitter.Commands.OpenSource.General;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Glitter.Commands.OpenSource;
 
 public static class GlitterExtensions
 {
     /// <summary>
-    /// Adds open source commands to the DI container.
+    /// Adds the core open source commands to the DI container.
     /// </summary>
     /// <param name="config"></param>
     /// <returns></returns>
     public static GlitterConfigurationBuilder AddOpenSourceCommands(this GlitterConfigurationBuilder config) =>
         AddOpenSourceCommands<SessionData>(config);
     /// <summary>
-    /// 
+    /// Adds the core open source commands to the DI container.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="config"></param>
     /// <returns></returns>
     public static GlitterConfigurationBuilder AddOpenSourceCommands<TSessionData>(this GlitterConfigurationBuilder config)
         where TSessionData : SessionData, new() =>
-        config.AddServices(services => services.AddSingleton<SessionData>(new TSessionData()));
+        config.AddServices(services => services.AddSingleton<SessionData>(new TSessionData()))
+              .AddCommand<UptimeCommand>()
+              .AddCommand<VersionCommand>();
 }
