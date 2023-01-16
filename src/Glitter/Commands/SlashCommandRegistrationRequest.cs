d4cf1da0906997e247d08993c9c6057fb479eeb4
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 21:28:29 -0600

    Created registration request for slash command.

---
+++ src/Glitter/Commands/SlashCommandRegistrationRequest.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Glitter.Commands;
+
+/// <summary>
+/// Represents a request to register a <see cref="SlashCommand"/>.
+/// </summary>
+public sealed class SlashCommandRegistrationRequest : IRequest
+{
+    /// <summary>
+    /// Gets the <see cref="SlashCommand"/> to register.
+    /// </summary>
+    public SlashCommand Command { get; }
+    /// <summary>
+    /// Creates a new <see cref="SlashCommandRegistrationRequest"/> instance.
+    /// </summary>
+    /// <param name="command">The <see cref="SlashCommand"/> to register.</param>
+    public SlashCommandRegistrationRequest(SlashCommand command) =>
+        Command = command;
+}
