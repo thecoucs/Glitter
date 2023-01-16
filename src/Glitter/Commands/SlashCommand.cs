58b3cf49ffcfd2a3e0f111bd6cc1deb0e7930f1a
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-15 21:28:19 -0600

    Created slash command.

---
+++ src/Glitter/Commands/SlashCommand.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Glitter.Commands;
+
+/// <summary>
+/// Represents a slash command that can be executed by a chatbot.
+/// </summary>
+public class SlashCommand : Command
+{
+    public SlashCommand(string key, string displayName, string description, ILogger logger) :
+        base(key, displayName, description, logger)
+    { }
+    protected override Task<CommandResponse> Work(CancellationToken cancellationToken) => throw new NotImplementedException();
+}
