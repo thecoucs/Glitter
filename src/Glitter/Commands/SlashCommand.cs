ec67db447644cca13c5e1ec550c181827ac1e2de
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-22 08:56:01 -0600

    Updated XML documentation for slash command.

--- src/Glitter/Commands/SlashCommand.cs
+++ src/Glitter/Commands/SlashCommand.cs
@@ -10,19 +10,19 @@ public abstract class SlashCommand : Command
     /// <summary>
     /// Creates a new <see cref="SlashCommand"/> instance.
     /// </summary>
-    /// <param name="key">The key for invoking the command.</param>
-    /// <param name="description">The description for the command.</param>
-    /// <param name="logger">The logger the command should utilize.</param>
+    /// <param name="key">The key for invoking the <see cref="SlashCommand"/>.</param>
+    /// <param name="description">The description for the <see cref="SlashCommand"/>.</param>
+    /// <param name="logger">The logger the <see cref="SlashCommand"/> should utilize.</param>
     public SlashCommand(string key, string description, ILogger logger) :
         base(key, description, logger)
     { }
     /// <summary>
     /// Creates a new <see cref="SlashCommand"/> instance.
     /// </summary>
-    /// <param name="key">The key for invoking the command.</param>
-    /// <param name="displayName">The display name for the command.</param>
-    /// <param name="description">The description for the command.</param>
-    /// <param name="logger">The logger the command should utilize.</param>
+    /// <param name="key">The key for invoking the <see cref="SlashCommand"/>.</param>
+    /// <param name="displayName">The display name for the <see cref="SlashCommand"/>.</param>
+    /// <param name="description">The description for the <see cref="SlashCommand"/>.</param>
+    /// <param name="logger">The logger the <see cref="SlashCommand"/> should utilize.</param>
     public SlashCommand(string key, string displayName, string description, ILogger logger) :
         base(key, displayName, description, logger)
     { }
