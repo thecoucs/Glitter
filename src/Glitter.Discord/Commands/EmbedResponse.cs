6881ed3ae98a1e1b6d7079febd4020406f034350
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-22 13:10:25 -0600

    Created embed response for Discord.

---
+++ src/Glitter.Discord/Commands/EmbedResponse.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+
+using Glitter.Commands;
+
+namespace Glitter.Discord.Commands;
+
+/// <summary>
+/// Represents a <see cref="CommandResponse"/> associated with an <see cref="Embed"/>.
+/// </summary>
+public class EmbedResponse : CommandResponse
+{
+    /// <summary>
+    /// The embed for the response.
+    /// </summary>
+    public Embed? Embed { get; set; }
+    /// <summary>
+    /// Creates a new <see cref="EmbedResponse"/> instance.
+    /// </summary>
+    /// <param name="message">The basic message for the response.</param>
+    public EmbedResponse(string message) :
+        base(message)
+    { }
+    /// <summary>
+    /// Creates a new <see cref="EmbedResponse"/> instance.
+    /// </summary>
+    /// <param name="embed">The embed for the response.</param>
+    /// <param name="message">The basic message for the response.</param>
+    public EmbedResponse(Embed embed, string message) :
+        base(message) =>
+        Embed = embed;
+}
