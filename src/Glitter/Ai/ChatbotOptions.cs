0141a988ac0eb1fd52a92e4de41ac2fc3d1f0e4d
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-22 16:42:56 -0600

    Created chatbot options.

---
+++ src/Glitter/Ai/ChatbotOptions.cs
@@ -0,0 +1,25 @@
+﻿namespace Glitter.Ai
+{
+    /// <summary>
+    /// Defines the basic options for a <see cref="Chatbot"/>.
+    /// </summary>
+    public class ChatbotOptions
+    {
+        /// <summary>
+        /// The prefix utilized to identify commands in a text-only based chat system.
+        /// </summary>
+        public string CommandPrefix { get; set; }
+        /// <summary>
+        /// The separator utilized to identify command arguments in a text-only based chat system.
+        /// </summary>
+        public string ParameterSeparator { get; set; }
+        /// <summary>
+        /// Creates a new <see cref="ChatbotOptions"/> instance.
+        /// </summary>
+        public ChatbotOptions()
+        {
+            CommandPrefix = "!";
+            ParameterSeparator = ",";
+        }
+    }
+}
