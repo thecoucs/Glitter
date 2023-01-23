e5c403eb867a17acc59331a1a02195ff230fc22e
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-22 20:18:34 -0600

    Added help command.

---
+++ src/Glitter.Commands.OpenSource/Baseline/HelpCommand.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Glitter.Commands.OpenSource.Baseline;
+
+public class HelpCommand : SlashCommand
+{
+    public HelpCommand(string key, string description, ILogger logger) : base(key, description, logger)
+    {
+    }
+    protected override Task<CommandResponse> Work(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
